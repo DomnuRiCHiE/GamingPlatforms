b1f8c64ceb61470dda6e0dc35e8d86f3c66f98c4
Robert <robert.burlacu03@gmail.com>
2024-04-26 18:27:28 +0300

    SpringBoot Setup

---
+++ src/main/java/dbms/project/GamingPlatforms/GamingPlatformsApplication.java
@@ -0,0 +1,13 @@
+package dbms.project.GamingPlatforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingPlatformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingPlatformsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dbms/project/GamingPlatforms/GamingPlatformsApplicationTests.java
@@ -0,0 +1,13 @@
+package dbms.project.GamingPlatforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingPlatformsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
