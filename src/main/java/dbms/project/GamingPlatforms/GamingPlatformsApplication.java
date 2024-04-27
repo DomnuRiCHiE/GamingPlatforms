d8f0fea0dac5b2347ffc0b6f66ae202b80ac7dfd
Robert <robert.burlacu03@gmail.com>
2024-04-27 03:22:16 +0300

    Java SpringBoot Stable TODO: Database and transactions

--- src/main/java/dbms/project/GamingPlatforms/GamingPlatformsApplication.java
+++ src/main/java/dbms/project/GamingPlatforms/GamingPlatformsApplication.java
@@ -2,8 +2,10 @@ package dbms.project.GamingPlatforms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("dbms.project.GamingPlatforms.Model")
 public class GamingPlatformsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GamingPlatformsApplication.class, args);
--- src/main/java/dbms/project/GamingPlatforms/Model/DLC.java
+++ src/main/java/dbms/project/GamingPlatforms/Model/DLC.java
@@ -11,7 +11,7 @@ import lombok.*;
 @Setter
 public class DLC {
     @Id
-    @Column
+    @Column(insertable = false, updatable = false)
     private Long id;
 
     @Column
